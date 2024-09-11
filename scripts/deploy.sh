7dde06cb1bbd3c0ef9a86e6af9405f4e7c1d1771
harishmahalingam <harish.m@atomedgesoft.com>
2024-09-11 15:03:17 +0530

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cd /home/ubuntu/app
-docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:leavetype
+cd /home/ubuntu/test-docker 
+# docker pull 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:leavetype
+docker-compose pull
+docker-compose down
+
+# Start the containers as defined in docker-compose.yml
+docker-compose up -d
+
 #docker stop my-postgres-container || true
 #docker rm my-postgres-container || true
-docker run -d --name my-postgres-container -e POSTGRES_USER=postgres POSTGRES_PASSWORD=admin123	 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:leavetype
+#docker run -d --name my-postgres-container -e POSTGRES_USER=postgres POSTGRES_PASSWORD=admin123	 270514764245.dkr.ecr.us-east-1.amazonaws.com/aura-postgres:leavetype
 
